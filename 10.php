f524b242a377da4c8c5df6e3f0bea326514509a6
Linas Žiedas <ziedas@gmail.com>
2014-03-26 16:29:25 +0200

    10 problem

---
+++ 10.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * SUMMATION OF PRIMES
+ *
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+
+* prime f-n steeled from http://stackoverflow.com/questions/16763322/a-formula-to-find-prime-numbers-in-a-loop
+ */
+
+$sum = 0;
+for($i=2; $i<2000000;$i++) {
+    if (isPrime($i)) {
+        $sum += $i;
+    }
+}
+
+function isPrime($num) {
+    //1 is not prime. See: http://en.wikipedia.org/wiki/Prime_number#Primality_of_one
+    if($num == 1)
+        return false;
+
+    //2 is prime (the only even number that is prime)
+    if($num == 2)
+        return true;
+
+    /**
+     * if the number is divisible by two, then it's not prime and it's no longer
+     * needed to check other even numbers
+     */
+    if($num % 2 == 0) {
+        return false;
+    }
+
+    /**
+     * Checks the odd numbers. If any of them is a factor, then it returns false.
+     * The sqrt can be an aproximation, hence just for the sake of
+     * security, one rounds it to the next highest integer value.
+     */
+    for($i = 3; $i <= ceil(sqrt($num)); $i = $i + 2) {
+        if($num % $i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+var_dump($sum);
