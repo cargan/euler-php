a90d5bf7b35d99ff9c581fde7a7550a84fe474e2
Linas Žiedas <ziedas@gmail.com>
2014-03-27 12:16:38 +0200

    11 problem and 9 upgrade

--- 9.php
+++ 9.php
@@ -16,8 +16,8 @@ Find the product abc.
 
 
 for ($a=1;$a<1000;$a++ ) {
-    for($b=1;$b<1000;$b++) {
-        for($c=1;$c<1000;$c++) {
+    for($b=$a;$b<1000;$b++) {
+        for($c=$b;$c<1000;$c++) {
             if ($a + $b + $c == 1000) {
                 if (pow($a, 2) + pow($b, 2) == pow($c,2)) {
                     var_dump($a, $b, $c, $a*$b*$c);
